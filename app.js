f38d5de1cac067bc9b37254e8066e4a2afa081fd
Nick Coleman <nick@itprojmgt.com>
2016-11-04 11:51:50 -0600

    cleaned up plan.txt

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // State Modification Functions
 var state = {
-	items: [],
-	counter: 0
+	items: []  // {name: "apples", checked: false}
 }
 
 function addItem(state, name) {
 	// add the item container
+	// creates new object
+	// adds/pushes item into state
 }
 
 function toggleChecked(state, item_num) {
@@ -23,16 +24,35 @@ function findItem(state, item_num) {
 
 // Render Functions
 
-function renderItem(state, element) {
+function renderItems(state, element) {
 	// render the new DOM
+	// loops over array of shopping list items
+	// creates a new html element and inserts it
 }
 
 // Event Listeners
-$(function(){
-	// add item listener
-	// check item listener
-	// delete item lisener
-	$("").on('click', function() {
 
-	})
+$(function(){
+	// delete item lisener
+	$("#additem").on('click', function() {
+		event.preventDefault();
+		// call addItem function passing the selected element/container
+		// render the new DOM via renderItem()
+	});
+
+	// check item listener
+	$("#checkItem").on('click', function() {
+		event.preventDefault();
+		// call toggledChecked pasing the state & found element
+		// render via renderItem()
+	});
+
+	// add item listener
+	#("#deleteItem").on('click', function() {
+		event.preventDefault();
+		// calling deleteItem() passing the state & clicked element
+		// render via renderItem
+	});
+
+
 });
