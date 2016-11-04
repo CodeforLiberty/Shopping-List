793c39697aa3470fa1fc2de5c80581da7aca1489
Nick Coleman <nick@itprojmgt.com>
2016-11-04 11:18:20 -0600

    Added app.js layout && cleaned up plan.txt

---
+++ app.js
@@ -0,0 +1,38 @@
+// State Modification Functions
+var state = {
+	items: [],
+	counter: 0
+}
+
+function addItem(state, name) {
+	// add the item container
+}
+
+function toggleChecked(state, item_num) {
+	// toggle selected item  line-through state 
+}
+
+function deleteItem(state, item_num) {
+	// remove item from state item arr
+}
+
+function findItem(state, item_num) {
+	// find the item in the state item arr
+	// return found item's ID
+}
+
+// Render Functions
+
+function renderItem(state, element) {
+	// render the new DOM
+}
+
+// Event Listeners
+$(function(){
+	// add item listener
+	// check item listener
+	// delete item lisener
+	$("").on('click', function() {
+
+	})
+});
